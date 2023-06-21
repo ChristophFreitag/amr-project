e0ea9d2698d0a90b10996c98b2853e501da91100
ChristophFreitag <freitag.christoph@gmx.de>
2023-06-22 01:43:43 +0200

    added missing files

--- src/install/my_package/share/my_package/launch/listener.launch.py
+++ src/install/my_package/share/my_package/launch/listener.launch.py
@@ -1,1 +1,10 @@
-/home/chris/dev_ws/src/my_package/launch/listener.launch.py
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='demo_nodes_py',
+            executable='listener'
+        )
+    ])
--- src/install/my_package/share/my_package/launch/talker.launch.py
+++ src/install/my_package/share/my_package/launch/talker.launch.py
@@ -1,1 +1,10 @@
-/home/chris/dev_ws/src/my_package/launch/talker.launch.py
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='demo_nodes_cpp',
+            executable='talker'
+        )
+    ])
